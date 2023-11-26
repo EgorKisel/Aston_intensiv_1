47fa7436803eec023fe0a221b7d81f44ee7d32ca
Egor Kisel <kisel1989@mail.ru>
2023-11-26 14:45:38 +0300

    При работе в фоне кнопки Pause/Next/Previous отображаются в уведомлении(notification), музыка из папки raw.

--- app/src/main/java/com/example/aston_intensiv_1/MainActivity.kt
+++ app/src/main/java/com/example/aston_intensiv_1/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.aston_intensiv_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var musicServiceIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        musicServiceIntent = Intent(this, MusicService::class.java)
+
+        val playButton = findViewById<Button>(R.id.playButton)
+        val pauseButton = findViewById<Button>(R.id.pauseButton)
+        val nextButton = findViewById<Button>(R.id.nextButton)
+        val prevButton = findViewById<Button>(R.id.prevButton)
+
+        playButton.setOnClickListener {
+            startService(musicServiceIntent.setAction("PLAY"))
+        }
+
+        pauseButton.setOnClickListener {
+            startService(musicServiceIntent.setAction("PAUSE"))
+        }
+
+        nextButton.setOnClickListener {
+            startService(musicServiceIntent.setAction("NEXT"))
+        }
+
+        prevButton.setOnClickListener {
+            startService(musicServiceIntent.setAction("PREVIOUS"))
+        }
+
+        startService(musicServiceIntent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(musicServiceIntent)
     }
 }
